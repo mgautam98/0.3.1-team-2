aff830aaa0348035bb062188f62fcea8c653f4be
Gautam Mishra <mishragautam96@gmail.com>
2020-06-03 01:08:03 +0530

    add doc string

--- src/imgProcess.jl
+++ src/imgProcess.jl
@@ -1,6 +1,11 @@
 using Images, LinearAlgebra,
 
+"""
+        _findBrightness(display, level)
 
+Finds the brightness of from the luminosity channel of HSL
+and returns the average of the pixels
+"""
 function _findBrightness(image)
     hslImage = HSL.(image)
     channels = channelview(float.(hslImage))
